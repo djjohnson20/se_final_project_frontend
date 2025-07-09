23edf4be0d135b4f153f7204277c5b606173b3d1
Donald Johnson <djjohnson380@gmail.com>
2025-07-09 11:26:51 -0700

    Update on Final Project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 import "./Footer.css";
+import Facebook from "../../assets/fb.png";
+import GitHub from "../../assets/github.png";
 
 function Footer() {
   return (
@@ -9,10 +11,33 @@ function Footer() {
         &copy; 2025 Supersite, Powered by News API
       </p>
       <div className="footer__links">
-        <Link>Home</Link>
-        <Link>TripleTen</Link>
-        <img src="" alt="" className="footer__github" />
-        <img src="" alt="" className="footer__facebook" />
+        <Link className="footer__home" to="/">
+          Home
+        </Link>
+        <a
+          href="https://tripleten.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer__tripleten"
+        >
+          TripleTen
+        </a>
+      </div>
+      <div className="footer__social">
+        <a
+          href="https://github.com/djjohnson20"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GitHub} alt="github" />
+        </a>
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Facebook} alt="facebook" />
+        </a>
       </div>
     </div>
   );
