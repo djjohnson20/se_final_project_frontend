6e77ad774af2c0ea78688ee6889e2bf0cc2e302e
Donald Johnson <djjohnson380@gmail.com>
2025-07-15 11:49:23 -0700

    Update on Final Project

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -26,6 +26,20 @@ const newsArticles = [
     description: "Tech",
     content: "We are the world! We will stand for what we believe!",
   },
+  {
+    source: {
+      name: "Biztoc.com",
+    },
+    author: "dogsinc.com",
+    title: "Did You Know? A Day in the Life of a Puppy Raiser",
+    url: "https://dogsinc.org/blog/puppy-raiser/day-life-puppy-raiser/?gad_source=1&gad_campaignid=20257872137&gbraid=0AAAAADmX_y_BCtvDa9-6k416UsNAuo7mn&gclid=CjwKCAjwyb3DBhBlEiwAqZLe5BNaP4XsXDOG_PztMP5AVqpNvqwxz6K3cOsobt834nZSWU57i1gTjhoCqAwQAvD_BwE",
+    urlToImage:
+      "https://t3.ftcdn.net/jpg/02/74/06/48/360_F_274064877_Tuq84kGOn5nhyIJeUFTUSvXaSeedAOTT.jpg",
+    publishedAt: "October 29th, 2020",
+    description: "Puppies",
+    content:
+      "Recruiting volunteers to raise our puppies is crucial to bringing our mission to life. We breed our pups right on our campus where they receive all of their vet care...",
+  },
 ];
 
 export { newsArticles };
